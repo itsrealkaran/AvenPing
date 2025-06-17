f1e19cceb3720f0bcbaead02cb38ff5ccc33c992
itsrealkaran <karansingh@duck.com>
2025-06-17 17:15:22 +0530

    fix: Body Minor UI

--- components/layout/body.tsx
+++ components/layout/body.tsx
@@ -9,8 +9,8 @@ interface BodyProps {
 export default function Body({ title, children }: BodyProps) {
   return (
     <div className="flex flex-col w-full h-full p-6 overflow-y-auto">
-      <div className="flex items-center gap-1 mb-6">
-        <h1 className="text-2xl font-300 text-gray-800">{title}</h1>
+      <div className="flex items-center gap-1 mb-4">
+        <h1 className="text-2xl font-400 text-gray-800">{title}</h1>
       </div>
       <div className="flex-1">{children}</div>
     </div>
