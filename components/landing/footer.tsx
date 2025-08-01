fc73bacf72ca9ec3171d5b9f93e2619d2084b7ab
itsrealkaran <karansingh@duck.com>
2025-08-02 01:56:57 +0530

    fix: Footer Design

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { motion } from "framer-motion"
 import { Facebook, Instagram, Linkedin, Youtube } from 'lucide-react'
 import Link from "next/link"
 import Logo from "@/components/landing/logo"
